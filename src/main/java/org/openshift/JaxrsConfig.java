f12eeaa50d9df9075dab3fd9d6e0e3987326afd6
Torey Lomenda <torey.lomenda@gmail.com>
2017-05-09 09:52:14 -0500

    Add Insult API to project.

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,12 @@
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by tlomenda on 5/9/17.
+ */
+@ApplicationPath("/api")
+public class JaxrsConfig extends Application {
+
+}
